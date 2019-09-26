01c7fe82b61106ecff0fcc949e192609fea606e2
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2019-09-26 21:37:46 +0200

    Some untested work on secondlayer processing. To be continued by implementation of two central subroutines and then some extensive testing is required prior to building the datetime layer on top of it.

--- impl2/test_auto/test_auto_secondlayer.c
+++ impl2/test_auto/test_auto_secondlayer.c
@@ -42,6 +42,7 @@ struct dcf77_high_level {
 	unsigned char private_line_lengths[DCF77_HIGH_LEVEL_LINES];
 	unsigned char private_line_current;
 	unsigned char private_line_cursor;
+	unsigned short private_leap_second_expected;
 	/* input */
 	enum dcf77_bitlayer_reading in_val;
 	/* output */
@@ -58,6 +59,8 @@ void dcf77_high_level_init(struct dcf77_high_level* ctx);
 void dcf77_high_level_process(struct dcf77_high_level* ctx);
 
 /* ------------------------------------------------[ Logic Implementation ]-- */
+static void shift_existing_bits_to_the_left(struct dcf77_high_level* ctx);
+
 void dcf77_high_level_init(struct dcf77_high_level* ctx)
 {
 	/* this is actually the same as a good-old reset function */
@@ -65,6 +68,7 @@ void dcf77_high_level_init(struct dcf77_high_level* ctx)
 	ctx->private_line_lengths[0] = 0;
 	ctx->private_line_current    = 0;
 	ctx->private_line_cursor     = 59;
+	ctx->private_leap_second_expected = 0; /* no leap second expected */
 	ctx->out_has_new_telegram    = 0;
 	/* initialize with epsilon */
 	memset(ctx->private_telegram_data, 0, DCF77_HIGH_LEVEL_MEM);
@@ -72,20 +76,22 @@ void dcf77_high_level_init(struct dcf77_high_level* ctx)
 
 void dcf77_high_level_process(struct dcf77_high_level* ctx)
 {
+	/* do nothing if no update */
 	if(ctx->in_val == DCF77_BIT_NO_UPDATE)
-		return; /* nothing if no update */
+		return;
 
+	/* write new input */
+	ctx->private_telegram_data[ctx->private_line_cursor / 4] |= 
+			(ctx->in_val << ((ctx->private_line_cursor % 4) * 2));
+	ctx->private_line_lengths[ctx->private_line_current]++;
+
+	/* decrease leap second expectation */
+	if(ctx->private_leap_second_expected != 0)
+		ctx->private_leap_second_expected--;
+
+	/* automaton case-specific handling */
 	switch(ctx->private_inmode) {
 	case IN_BACKWARD:
-		/* write new input */
-		/*
-		TODO CSTAT NOW WE HAVE THE SALAD:
-		THIS SHOULD WORK, BUT: VAL NEEDS TO BE IN THE SECONDLAYERS FORMAT
-		WHICH IS AS OF NOW DIFFERENT TO THE LOW_LEVEL FORMAT. ALIGN THESE
-		TWO FORMATS S.T. WE CAN DIRECTLY IMPORT THE DATA HERE!!!
-		*/
-		ctx->private_telegram_data[ctx->private_line_cursor / 4] |= 
-			(ctx->in_val << ((ctx->private_line_cursor % 4) * 2));
 		if(ctx->in_val == DCF77_BIT_NO_SIGNAL) {
 			/*
 			 * no signal might indicate: end of minute.
@@ -95,9 +101,8 @@ void dcf77_high_level_process(struct dcf77_high_level* ctx)
 			ctx->private_inmode          = IN_FORWARD;
 			ctx->private_line_current    = 1;
 			ctx->private_line_cursor     = 0;
-			ctx->private_line_lengths[0]++;
 			ctx->private_line_lengths[1] = 0;
-		} else if(ctx->private_line_lengths[0] == 59) {
+		} else if(ctx->private_line_lengths[0] == 60) {
 			/*
 			 * we processed 59 bits before, this is the 60. without
 			 * an end of minute. This is bad data. Discard
@@ -115,11 +120,74 @@ void dcf77_high_level_process(struct dcf77_high_level* ctx)
 			 * Now that we have added our input, move bits and
 			 * increase number of bits in line.
 			 */
-			ctx->private_line_lengths[0]++;
-			/* TODO CSTAT IMPLEMENT AND CALL MOVE BACKWARDS ROUTINE */
+			shift_existing_bits_to_the_left(ctx);
 		}
 		break;
 	case IN_FORWARD:
+		if(ctx->private_line_cursor == 59) {
+			/*
+			 * we just wrote the 60. bit (at index 59).
+			 * this means we are at the end of a minute.
+			 * the current value should reflect this or
+			 * it might possibly be a leap second (if announced).
+			 */
+			if(ctx->in_val == DCF77_BIT_NO_SIGNAL) {
+				/*
+				 * no signal in any case means this is our
+				 * end-of-minute marker
+				 */
+				/* TODO N_IMPL invoke process_telegrams() and advance to next "line" */
+			} else if(ctx->in_val == DCF77_BIT_0 &&
+					ctx->private_leap_second_expected > 0) {
+				/*
+				 * this was possibly the expected leap second
+				 * which is marked by an additional 0-bit.
+				 * In this very case, the cursor position 60
+				 * becomes available for processing.
+				 */
+				ctx->private_line_cursor++;
+			} else {
+				/*
+				 * A signal was received but in this specific
+				 * data arragngement, there should definitely
+				 * have been a `NO_SIGNAL` at this place.
+				 * We need to reorganize the datastructure
+				 * to align to the "reality".
+				 */
+				/* TODO N_IMPL / invoke recompute_eom(), afterwards the line will likely not be 100% full, but cursor reorganization is handled by compute_eom... */
+			}
+		} else {
+			/*
+			 * If we are not near the end, just quietly append
+			 * received bits, i.e. advance cursor (the rest of
+			 * that is already implemented above).
+			 */
+			ctx->private_line_cursor++;
+		}
 		break;
+	}
+}
+
+static void shift_existing_bits_to_the_left(struct dcf77_high_level* ctx)
+{
+	unsigned char current_byte;
+
+	/* TODO z not sure if this -1 makes sense here */
+	for(current_byte = (60 - ctx->private_line_lengths[0] - 1) / 4;
+					current_byte < 16; current_byte++) {
+		/*
+		 * split off the first two bits (which get shifted out)
+		 * and put them in the previous byte if there is a previous
+		 * byte (otherwise discard them silently).
+		 */
+		if(current_byte > 0)
+			ctx->private_telegram_data[current_byte - 1] |= (
+				(ctx->private_telegram_data[current_byte] &
+				0xc0) >> 6);
+		/*
+		 * shift current byte two leftwards, making space for one
+		 * datapoint
+		 */
+		ctx->private_telegram_data[current_byte] <<= 2;
 	}
 }
