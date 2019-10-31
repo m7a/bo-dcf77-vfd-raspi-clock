01a5d0345b190ac89d19d475de797ee6a629fefa
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2019-10-31 21:29:01 +0100

    Intermediate commit. Add some idea for a part of the output reordering procedure (now complete with in and output line handling). It needs some minor tweaks, e.g. correct final variable declaration and the associated procedures for determinining the move amount and leapsec lines need to be added form paper to the code.

--- impl2/test_auto/test_auto_secondlayer.c
+++ impl2/test_auto/test_auto_secondlayer.c
@@ -660,7 +660,7 @@ static void recompute_eom(struct dcf77_high_level* ctx)
 	 */
 }
 
-static void move_entries_backwards(struct dcf77_high_level* ctx,
+static void move_entries_backwards_old(struct dcf77_high_level* ctx,
 							unsigned char mov)
 {
 	unsigned char mov_bytes   = mov / 4; /* how many bytes move backwards */
@@ -723,6 +723,138 @@ static void move_entries_backwards(struct dcf77_high_level* ctx,
 			ctx->private_telegram_data[bc - mov_bytes] = upper_low;
 		dist++;
 	}
+}
+
+#define MOVENTRIES_ADVANCE_OUTPUT_CURSOR \
+	{ \
+		if(pol == 14) { \
+			pol = 0; \
+			if(next_produce_leapsec_marker) { \
+				next_produce_leapsec_marker   = 0; \
+				ctx->private_line_lengths[ol] = 61; \
+			} else { \
+				ctx->private_line_lengths[ol] = 60; \
+			} \
+			prevoll = ctx->private_line_lengths[ol]; \
+			ol = nextl(ol); \
+		} else { \
+			pol++; \
+		} \
+	}
+
+/* @param mov: aka. v */
+static void move_entries_backwards(struct dcf77_high_level* ctx,
+		unsigned char mov, unsigned char in_line_holding_leapsec_marker)
+{
+	unsigned char il0; /* line in first inclusive */
+
+	unsigned char previll; /* previous input line length */
+	unsigned char il;      /* current input line */
+	unsigned char ill;     /* current input line length */
+	unsigned char pil;     /* position in input line */
+	unsigned char bytes_to_proc;
+
+	unsigned char readib;  /* read input byte position in memory */
+	/*
+	 * upper bytes will be moved "forward" and placed at a low position
+	 * within the byte
+	 */
+	unsigned char upper_low;
+	/*
+	 * lower bytes will be moved "backward" and placed at a high position
+	 * within the byte
+	 */
+	unsigned char lower_up;
+
+	unsigned char next_produce_leapsec_marker; /* bool */
+	unsigned char produce_leap; /* bool */
+	unsigned char send_back_offset;
+	unsigned char ol;
+	unsigned char wrpos;
+
+	unsigned char bytes_proc = 0;
+
+	char prevoll = -1;     /* previous output line length */
+	unsigned char pol = 0; /* position output line */
+
+	unsigned char mov_entries = (mov % 4);
+	unsigned char mov_bytes_initial = mov / 4;
+
+	/* -- Bestimme l0 -- */
+
+	/*
+	 * start from the first line in buffer.
+	 * This is the first line following from the current which is not
+	 * empty.
+	 */
+	for(il0 = nextl(ctx->private_line_current);
+			ctx->private_line_lengths[il0] == 0 &&
+			il0 != ctx->private_line_current; il0 = nextl(il0));
+
+	ol = il0 * DCF77_HIGH_LEVEL_LINE_BYTES;
+
+	/* -- Hauptprozedur -- */
+	next_produce_leapsec_marker = 0;
+	il = il0;
+	do {
+		next_produce_leapsec_marker |=
+					(il == in_line_holding_leapsec_marker);
+		ill = ctx->private_line_lengths[il];
+		/* ctx->private_line_lengths[il] = 0; * superflous? */
+
+		/* skip empty input lines */
+		if(ill == 0)
+			continue;
+
+		/* 
+		 * 0..14 werden immer verarbeitet
+		 * 15    nur bei leapsec und dann von einer separaten Logik
+		 * ob man <60 oder <61 schreibt ist egal.
+		 */
+		bytes_to_proc = ill / 4 + ((ill % 4) != 0 && ill < 60);
+		for(pil = 0; pil < bytes_to_proc; pil++) {
+			/* -- Verarbeiteprozedur: Lies Eingabe -- */
+			readib = pil + il * DCF77_HIGH_LEVEL_LINE_BYTES;
+			upper_low = ctx->data[readib] >> (2 * mov_entries);
+			shf = 8 * 2 - mov_entries;
+			lower_up = ((0xff >> shf) & ctx->data[readib]) << shf;
+			if(pil == 0 && previll == 61) {	
+				/* leap_in = X (might also want to read) */
+				upper_low = (upper_low << 2) |
+							DCF77_BIT_NO_SIGNAL;
+				mov--;
+				mov_entries = mov % 4;
+				if(mov_entries == 0)
+					MOVENTRIES_ADVANCE_OUTPUT_CURSOR
+			}
+			/* -- Verarbeiteprozedur: Schreibe Ausgabe -- */
+			produce_leap = ((prevoll == 61) && (pol == 0));
+			send_back_offset = (produceleap || pol == 0)? 2: 1;
+			wrpos = pol + ol * DCF77_HIGH_LEVEL_LINE_BYTES;
+			if(bytes_proc >= (mov_bytes_initial + send_back_offset))
+				ctx->data[wrpos - send_back_offset] |= lower_up;
+
+			if(produce_leap) {
+				if(bytes_proc >= (mov_bytes_initial + 1))
+					ctx->data[wrpos - 1] =
+							DCF77_BIT_NO_SIGNAL;
+				upper_low <<= 2; /* cancel lowermost entry */
+			}
+			ctx->data[wrpos] |= upper_low;
+			INC_SAT(bytes_proc);
+			if(produce_leap) {
+				mov++;
+				mov_bytes = mov % 4;
+				if(mov_bytes == 0)
+					continue; /* skip addr inc */
+			}
+			MOVENTRIES_ADVANCE_OUTPUT_CURSOR
+		}
+		previll = ill;
+	} while((il = nextl(il)) != l0);
+	
+	/* -- Abschließende Aktualisierungen -- */
+	/* TODO SET LAT LINE LENGTH, THEN SET CURSOR; ctx->private_line_lengths[ol] = ... */
 }
 
 static void postprocess(struct dcf77_high_level* ctx,
