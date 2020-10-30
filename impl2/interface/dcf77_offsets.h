0b3bcaa87ab92f238322dc59fbeedfed568a4ff6
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2020-10-30 22:03:58 +0100

    Reorganize DEV res. Add dcf77_offsets.h (to be used widely). Started to create a dcf77_proc_check_bcd_corret_telegram.c for the purpose of checking telegram validity. Implementation of that procedure needs to support cross-line reading of data and is thus complicated. Also, procedure reorganize_eom() will need to call this new function once available. This is also not trivial because the number of invocations should be kept as minimal as possible. In a generalized case we might also add responsibility for EOM detection to the dcf77_proc_check_bcd_correct_telgram as to _simplify_ and streamline reorganize_eom().

---
+++ impl2/interface/dcf77_offsets.h
@@ -0,0 +1,20 @@
+/* TODO use this exensively (mainly xeliminate does not use it yet) */
+
+#define DCF77_OFFSET_BEGIN_OF_MINUTE      0  /*     0 */
+#define DCF77_OFFSET_DAYLIGHT_SAVING_TIME 17 /* 17-18 */
+#define DCF77_OFFSET_BEGIN_TIME           20
+#define DCF77_OFFSET_MINUTE_ONES          21
+#define DCF77_OFFSET_MINUTE_TENS          25
+#define DCF77_OFFSET_PARITY_MINUTE        28
+#define DCF77_OFFSET_HOUR_ONES            29
+#define DCF77_OFFSET_HOER_TENS            33
+#define DCF77_OFFSET_PARITY_HOUR          35
+#define DCF77_OFFSET_DAY_ONES             36
+#define DCF77_OFFSET_DAY_TENS             40
+#define DCF77_OFFSET_DAY_OF_WEEK          42
+#define DCF77_OFFSET_MONTH_ONES           45
+#define DCF77_OFFSET_MONTH_TENS           49
+#define DCF77_OFFSET_YEAR_ONES            50
+#define DCF77_OFFSET_YEAR_TENS            57
+#define DCF77_OFFSET_PARITY_DATE          58
+#define DCF77_OFFSET_ENDMARKER_REGULAR    59
