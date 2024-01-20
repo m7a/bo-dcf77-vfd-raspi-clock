656c39385de9eec68ff46476230b1187e7fcd529
Linux-Fan, Ma_Sys.ma <Ma_Sys.ma@web.de>
2024-01-20 19:49:45 +0100

    Version deployed to device. Improved Bitlayer handling (and added some comments to explain the reasoning behind it).

--- src/dcf77_bitlayer.adb
+++ src/dcf77_bitlayer.adb
@@ -20,6 +20,7 @@ package body DCF77_Bitlayer is
 		End_Of_Sec:   constant Time := Ctx.Start_Of_Sec + Second_In_Us;
 		End_Of_Slice: constant Time := Ctx.Start_Of_Slice +
 								Delay_Us_Target;
+		Has_Reading:   Boolean := False;
 		Time_Now:      Time := Ctx.LL.Get_Time_Micros;
 		Signal_Length: Time;
 		Signal_Begin:  Time;
@@ -51,10 +52,14 @@ package body DCF77_Bitlayer is
 		-- the clock run faster than intended.
 		if Ctx.LL.Read_Interrupt_Signal(Signal_Length, Signal_Begin)
 									then
+			-- These signal lengths in general identify a valid
+			-- reading
 			if Signal_Length in 50_000 .. 140_000 then
-				R := Bit_0;
+				Has_Reading := True;
+				R           := Bit_0;
 			elsif Signal_Length in 150_000 .. 250_000 then
-				R := Bit_1;
+				Has_Reading := True;
+				R           := Bit_1;
 			else
 				R := No_Update;
 				Ctx.LL.Log("Unidentified: " & Time'Image(
@@ -62,20 +67,32 @@ package body DCF77_Bitlayer is
 				DCF77_Functions.Inc_Saturated(Ctx.Unidentified,
 									9999);
 			end if;
-			if (R = Bit_0 or R = Bit_1) and (Signal_Begin -
+			-- We have to discard the reading if we already had
+			-- one within the preceding 750 (800) ms.
+			if Has_Reading and (Signal_Begin -
 					Ctx.Preceding_Signal) < 750_000 then
-				R := No_Update;
+				Has_Reading := False;
+				R           := No_Update;
 				Ctx.LL.Log("Discard: " &
 						Time'Image(Signal_Length));
 				DCF77_Functions.Inc_Saturated(Ctx.Discarded,
 									9999);
 			else
+			-- If it is all good, record the signal begin as valid
+			-- time to compare subsequent signals against
 				Ctx.Preceding_Signal := Signal_Begin;
 			end if;
-		else
+		end if;
+		-- If no valid signal was produced (any of no interrupt or
+		-- wrong signal length or discarded signal) then check if we
+		-- have exceeded 1sec and switch the second counter despite not
+		-- having received anything valuable. Otherwise say “No Update”.
+		if not Has_Reading then
 			R := (if Time_Now > (End_Of_Sec + Delay_Us_Epsilon)
 						then No_Signal else No_Update);
 		end if;
+		-- If anything (even No_Signal end of second) was output,
+		-- forward internal reference by one second.
 		if R /= No_Update then
 			Ctx.Start_Of_Sec := End_Of_Sec;
 		end if;
